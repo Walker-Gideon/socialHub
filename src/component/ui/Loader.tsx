4dff58c5aed83f39d352304c849befa1dbcbdcc0
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-01 20:46:12 +0000

    adjustment in the loader component

--- src/component/ui/Loader.tsx
+++ src/component/ui/Loader.tsx
@@ -1,7 +1,7 @@
 export default function Loader() {
     return (
-      <main className="min-h-screen bg-gradient-to-br from-background to-card flex items-center justify-center">
-        <div className="text-foreground">Loading...</div>
+      <main className={"min-h-screen bg-gradient-to-br from-background to-card flex items-center justify-center"}>
+        <div className={"text-foreground"}>Loading...</div>
       </main>
     )
 }
